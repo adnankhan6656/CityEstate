7be6508bbd5f001e0075c81e4cdedecb5640653e
Adnankhan <pathanadnan160@gmail.com>
2023-10-13 09:12:43 -0500

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoute from "./routes/userRoute.js";
 import authRouter from './routes/auth.route.js';
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 async function connectToMongoDB() {
     try {
@@ -16,6 +17,7 @@ async function connectToMongoDB() {
 }
 
 connectToMongoDB();
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -28,6 +30,12 @@ app.listen(3000, () => {
 app.use('/api/user',userRoute);
 app.use('/api/auth', authRouter);
 app.use('/api/listing',listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 app.use((err, req, res, next) => {
